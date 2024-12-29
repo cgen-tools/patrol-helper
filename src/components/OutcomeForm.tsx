add6808deb3caa3530d61ebbe4dffe37a6064251
Lara <63805048+larkgz@users.noreply.github.com>
2024-12-28 23:03:23 -0500

    feat: most static fields for outcome form

--- src/components/OutcomeForm.tsx
+++ src/components/OutcomeForm.tsx
@@ -1,8 +1,73 @@
+import { Checkbox, Fieldset, Group, MultiSelect, Radio, Space, Text } from "@mantine/core";
+import { SKILLS, TRAITS } from "../resources";
+
 function OutcomeForm() {
   return (
-    <>
-      
-    </>
+    <Fieldset>
+      <Radio.Group defaultValue="standard" label="Outcome Type">
+        <Group>
+          <Radio value="success" label="Success" />
+          <Radio value="failure" label="Failure" />
+          <Radio value="antag_success" label="Antagonize Success" />
+          <Radio value="antag_failure" label="Antagonize Failure" />
+        </Group>
+      </Radio.Group>
+      <Space h="xs" />
+
+      <Text mt="sm" fw={500} size="sm">
+        s_c Minimum Skill Level Requirements (any can be true)
+      </Text>
+      <MultiSelect placeholder="Select skills" data={SKILLS} searchable />
+
+      <Text mt="sm" fw={500} size="sm">
+        s_c Required Traits (any can be true)
+      </Text>
+      <MultiSelect placeholder="Select trait" data={TRAITS} searchable />
+
+      <Checkbox.Group mt="sm" label="Cats allowed to be s_c">
+        <Group>
+          <Checkbox value="p_l" label="p_l" />
+          <Checkbox value="r_c" label="r_c" />
+          <Checkbox value="app1" label="app1" />
+          <Checkbox value="app2" label="app2" />
+          <Checkbox value="not_pl_rc" label="Any cat except p_l and r_c" />
+          <Checkbox value="any" label="Any cat" />
+        </Group>
+      </Checkbox.Group>
+
+      <Checkbox.Group mt="sm" label="s_c Additional Requirements">
+        <Group>
+          <Checkbox value="adult" label="s_c is not an apprentice" />
+          <Checkbox value="app" label="s_c is an apprentice" />
+          <Checkbox value="healer" label="s_c is a medicine cat or medicine cat apprentice" />
+        </Group>
+      </Checkbox.Group>
+      <Space h="xs" />
+
+      <Checkbox.Group mt="sm" label="Lost Cats">
+      <Group>
+          <Checkbox value="p_l" label="p_l" />
+          <Checkbox value="r_c" label="r_c" />
+          <Checkbox value="s_c" label="s_c" />
+          <Checkbox value="app1" label="app1" />
+          <Checkbox value="app2" label="app2" />
+          <Checkbox value="patrol" label="The entire patrol" />
+          <Checkbox value="multi" label="Multiple cats, but not the whole patrol" />
+        </Group>
+      </Checkbox.Group>
+
+      <Checkbox.Group mt="sm" label="Dead Cats">
+      <Group>
+          <Checkbox value="p_l" label="p_l" />
+          <Checkbox value="r_c" label="r_c" />
+          <Checkbox value="s_c" label="s_c" />
+          <Checkbox value="app1" label="app1" />
+          <Checkbox value="app2" label="app2" />
+          <Checkbox value="patrol" label="The entire patrol" />
+          <Checkbox value="multi" label="Multiple cats, but not the whole patrol" />
+        </Group>
+      </Checkbox.Group>
+    </Fieldset>
   )
 }
 
